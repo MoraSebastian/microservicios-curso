7016377d7b4432f5d242b7f81cdbe150c64734a9
MoraSebastian <morasabogalsebastian@gmail.com>
2022-11-08 15:31:42 -0500

    Rest template - open feign

---
+++ usuario-service/src/main/java/com/usuario/service/entidades/Usuario.java
@@ -0,0 +1,23 @@
+
+package com.usuario.service.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    
+    private String nombre;
+    private String email;
+    
+    
+}
